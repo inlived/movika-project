cca7d2c85dd9ff54381982134662ba2b93f652fb
Darya Sidorenko <darya20012911@yandex.ru>
2022-06-05 22:22:07 +0200

    version2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 import './App.css';
-import Navbar from "./components/navbar/Navbar";
 import MyButton from "./components/button/MyButton";
 
 function App() {
